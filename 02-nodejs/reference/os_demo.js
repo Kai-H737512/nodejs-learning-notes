a5c37f1f46b449b24914b62c22ab3998ed65b1c4
Kai Huang <z5467942@ad.unsw.edu.au>
2025-07-05 23:22:27 +1000

    learn os module

---
+++ 02-nodejs/reference/os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU
+console.log(os.arch())
+
+// CPU core info
+console.log(os.cpus())
+
+// Freememory
+console.log(os.freemem())
+
+// Total memory
+console.log(os.totalmem())
+
+// Home dir
+console.log(os.homedir())
